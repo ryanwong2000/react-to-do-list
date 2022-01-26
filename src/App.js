af93159a830c4e74df5ca98186ab5db71b3fe880
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-01-25 20:29:58 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import TodoList from './TodoList';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
   const [todos, setTodos] = useState([]); //obj destructuring
+  const todoNameRef = useRef();
 
-  function handleAddTodo(e) {}
+  //Store todos in localStorage
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  //getting todos from localStorage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  //change the checked status of a todo
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: true }];
+    });
+    todoNameRef.current.value = null; //reset text box input
+  }
+
+  function handleClear(e) {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
 
   return (
     <>
-      <TodoList todoList={todos} />
+      <TodoList todoList={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
-      <button onCLick={handleAddTodo}> Add To Do</button>
-      <button>Clear Completed Tasks</button>
-      <div>0 left to do</div>
+      <button onClick={handleAddTodo}> Add To Do</button>
+      <button onClick={handleClear}>Clear Completed Tasks</button>
+      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
     </>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Todo from './Todo.js';
 
-export default function TodoList({ todoList }) {
+export default function TodoList({ todoList, toggleTodo }) {
   return todoList.map((todo) => {
-    return <Todo key={todo.id} todo={todo} />; //'key'is a unique key that can identify the component
+    return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />; //'key'is a unique key that can identify the component
   });
 }
